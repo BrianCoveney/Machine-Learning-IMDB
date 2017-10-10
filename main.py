2a33ae724f9472fb057dbf935472215f35daad8e
Brian Coveney <coveneybrian@gmail.com>
2017-10-10 08:47:05 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+from collections import Counter
+
+path_neg = '/home/brian/Desktop/4thYr_MachineLearning/Assignment/LargeIMDB/neg'
+
+listing = os.listdir(path_neg)
+
+
+def read_words(list_files):
+
+    vocab_list = []
+
+    for eachFile in list_files:
+        f = open(path_neg + "/" + eachFile, "r")
+        words = f.read().split()
+        f.close()
+
+        vocab_list.append(words)
+
+    return vocab_list
+
+
+def print_words():
+    words = read_words(listing)
+
+    for w in words:
+        freq = Counter(w)
+        print(freq)
+
+
+def main():
+
+    read_words(listing)
+
+    print_words()
+
+
+if __name__ == "__main__":
+    main()
