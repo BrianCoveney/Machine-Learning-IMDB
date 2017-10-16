e0c97b7f081716bd540d4f0afc4729c32b3af7a7
Brian Coveney <coveneybrian@gmail.com>
2017-10-16 20:59:26 +0100

    Calculate the prior probabilities

--- main.py
+++ main.py
@@ -1,43 +1,51 @@
 import os
 import re
 
-# path_pos = '/home/brian/Desktop/4thYr_MachineLearning/Assignment/LargeIMDB/pos'
-# path_neg = '/home/brian/Desktop/4thYr_MachineLearning/Assignment/LargeIMDB/neg'
-# listing_pos = os.listdir(path_pos)
-# listing_neg = os.listdir(path_neg)
+path_pos = '/home/brian/Desktop/4thYr_MachineLearning/Assignment/smallTest/pos'
+listing_pos = os.listdir(path_pos)
+path_neg = '/home/brian/Desktop/4thYr_MachineLearning/Assignment/smallTest/neg'
+listing_neg = os.listdir(path_neg)
 
-example_pos_path = 'res/pos'
-example_pos_listings = os.listdir((example_pos_path))
+word_dict = {}
+word_pos_dict = {}
+word_neg_dict = {}
 
 
-def store_unique_words(listing, path):
-    word_dict ={}
-
+def readWords(listing, path):
     for eachFile in listing:
         f = open(path + "/" + eachFile, "r", encoding='utf8')
         for line in f:
             for word in re.findall(r'[\w]+', line.lower()):
-                word_dict[word] = word_dict.setdefault(word, 0) + 1
-
-        # print each file, word and word freq -> file1 'that': 5 | file2 'that': 9 | file3 'that': 20
-        # print(f)
-        # print(word_dict)
-
-    # print unique words and freq -> 'that': 20
-    print(word_dict)
+                word_dict[word] = word_dict.get(word, 0.0) + 1.0
 
     return word_dict
 
 
+def calcProbability(wd_dict, words_type_dict):
+    for k, v in wd_dict.items():
+        # Calculate the prior probabilities
+        words_type_dict[k] = wd_dict[k] / sum(wd_dict.values())
+
+        # Todo: Calculations of the probabilities in the multinominal model using laplace smoothing
+
+    return word_pos_dict
+
+
+def createPositiveWordDict(listings, path):
+    pos_wd_dict = readWords(listings, path)
+    prob_pos_word = calcProbability(pos_wd_dict, word_pos_dict)
+    print("Positive", prob_pos_word, "\n")
+
+
+def createNegativeWordDict(listings, path):
+    neg_wd_dict = readWords(listings, path)
+    prob_neg_word = calcProbability(neg_wd_dict, word_neg_dict)
+    print("Negative", prob_neg_word)
+
+
 def main():
-
-    # Positive review words
-    # store_unique_words(listing_pos, path_pos)
-
-    # Negative review words
-    # store_unique_words(listing_neg, path_neg)
-
-    store_unique_words(example_pos_listings, example_pos_path)
+    createPositiveWordDict(listing_pos, path_pos)
+    createNegativeWordDict(listing_neg, path_neg)
 
 
 if __name__ == "__main__":
